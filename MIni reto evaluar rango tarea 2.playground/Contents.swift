1b9081eb7d588a7eca40020216138fd6d318be73
israelpiz <macbookpro@israelpizmacbookpro.local>
2016-02-07 13:31:02 -0600

    intenté agregar de nuevo el playground...

---
+++ MIni reto evaluar rango tarea 2.playground/Contents.swift
@@ -0,0 +1,49 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+/*
+
+Programa un playground que evalúe un rango de números en base a 4 reglas de impresión.
+Generar un rango de 0 a 100, incluye el número 100 en el rango.
+Tienes que iterar el rango completo, utilizando la sentencia for para obtener cada número del rango y aplicar las siguientes reglas de impresión.
+
+Al evaluar cada número debes aplicar las siguientes reglas:
+- Si el número es divisible entre 5, imprime: # el número  + “Bingo!!!”
+- Si el número es par, imprime: # el número + “par!!!”
+- Si el número es impar, imprime: # el número + “impar!!!”
+- Si el número se encuentra dentro de un rango del 30 al 40, imprime: # el número +  “Viva Swift!!!”
+Debes de usar la interpolación de variables para realizar la impresión de cada número.
+La salida de mensajes dejes tenerla en la consola.
+El proyecto deberá estar en la cuenta de GitHub del alumno
+
+*/
+
+var rangoNumeros = 0...100
+
+for i in rangoNumeros{
+
+    switch i {
+    case 0...29:
+        if i % 5 == 0{
+            print("\(i)\t Bingo!!!")
+        }else if i % 2 == 0{
+            print("\(i)\t par!!!")
+        }else{
+            print("\(i)\t impar!!!")
+        }
+    case 30...40:
+        print("\(i)\t Viva Swift!!!")
+    case 41...100:
+        if i % 5 == 0{
+            print("\(i)\t Bingo!!!")
+        }else if i % 2 == 0{
+            print("\(i)\t Par!!!")
+        }else{
+            print("\(i)\t Impar!!!")
+        }
+    default:
+        print("")
+    }
+}
+
